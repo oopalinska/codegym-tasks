128e0c4679b1edde5259b91168267f8107e9e0bc
OlgaOpalinska <olgaopalinska@wp.pl>
2021-02-26 15:56:35 +0100

    Add task2106 on equals & hashcode

---
+++ src/task2106_equals_and_hashcode/Solution.java
@@ -0,0 +1,57 @@
+package task2106_equals_and_hashcode;
+
+import java.util.Date;
+
+/*
+This task was about fixing a bug in overridden equals and hashcode methods
+
+I could use this code later as a reminder of how to override them properly (I'll maybe change it in the future).
+
+*/
+
+public class Solution {
+    private int anInt;
+    private String string;
+    private double aDouble;
+    private Date date;
+    private Solution solution;
+
+    public Solution(int anInt, String string, double aDouble, Date date, Solution solution) {
+        this.anInt = anInt;
+        this.string = string;
+        this.aDouble = aDouble;
+        this.date = date;
+        this.solution = solution;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Solution)) return false;
+
+        Solution solution1 = (Solution) o;
+
+        if (Double.compare(solution1.aDouble, aDouble) != 0) return false;
+        if (anInt != solution1.anInt) return false;
+        if (date != null ? !date.equals(solution1.date) : solution1.date != null) return false;
+        if (solution != null ? !solution.equals(solution1.solution) : solution1.solution != null) return false;
+        if (string != null ? !string.equals(solution1.string) : solution1.string != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = anInt;
+        temp = aDouble != +0.0d ? Double.doubleToLongBits(aDouble) : 0L;
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (solution != null ? solution.hashCode() : 0);
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
