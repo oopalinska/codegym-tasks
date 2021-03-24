cbf414e54cd5d2a0720b6b3059a838a4f46c2918
OlgaOpalinska <olgaopalinska@wp.pl>
2021-03-24 13:52:35 +0100

    Add task 3304 - JSON serialization

---
+++ src/task3304_json_serialization/Solution.java
@@ -0,0 +1,46 @@
+package task3304_json_serialization;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+/*
+Using JSON to convert from one class to another
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Second s = (Second) convertOneToAnother(new First(), Second.class);
+        First f = (First) convertOneToAnother(new Second(), First.class);
+    }
+
+    public static Object convertOneToAnother(Object one, Class resultClassObject) throws IOException {
+        StringWriter writer = new StringWriter();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(MapperFeature.USE_ANNOTATIONS);
+        mapper.writeValue(writer, one);
+        String s = writer.toString();
+        StringReader reader = new StringReader(s);
+        return mapper.readValue(reader, resultClassObject);
+    }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,  property="className")
+    @JsonSubTypes(@JsonSubTypes.Type(value=First.class,  name="first"))
+    public static class First {
+        public int i;
+        public String name;
+    }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,  property="className")
+    @JsonSubTypes(@JsonSubTypes.Type(value=Second.class, name="second"))
+    public static class Second {
+        public int i;
+        public String name;
+    }
+}
