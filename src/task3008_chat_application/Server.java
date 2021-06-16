24e31ace0f02f7caf42cbaaea883f34a94b9aefc
OlgaOpalinska <olgaopalinska@wp.pl>
2021-06-16 14:24:20 +0200

    Add notifyUsers, serverMainLoop and run methods in Server class

--- src/task3008_chat_application/Server.java
+++ src/task3008_chat_application/Server.java
@@ -40,6 +40,26 @@ public class Server {
             this.socket = socket;
         }
 
+        public void run() {
+            ConsoleHelper.writeMessage(
+                    "A new connection has been established. Address: "
+                            + socket.getRemoteSocketAddress());
+            String clientName = null;
+            try (Connection connection = new Connection(socket)){
+                clientName = serverHandshake(connection);
+                sendBroadcastMessage(new Message(MessageType.USER_ADDED, clientName));
+                notifyUsers(connection, clientName);
+                serverMainLoop(connection, clientName);
+                ConsoleHelper.writeMessage("Connection with the remote address is closed.");
+            } catch (IOException | ClassNotFoundException e) {
+                ConsoleHelper.writeMessage("An  error occurred while communicating with the remote address.");
+            }
+            if(clientName != null && connectionMap.containsKey(clientName)) {
+                connectionMap.remove(clientName);
+                sendBroadcastMessage(new Message(MessageType.USER_REMOVED, clientName));
+            }
+        }
+
         private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
 
             while (true) {
@@ -57,6 +77,25 @@ public class Server {
                 connectionMap.put(userName, connection);
                 connection.send(new Message(MessageType.NAME_ACCEPTED));
                 return userName;
+            }
+        }
+
+        private void notifyUsers(Connection connection, String userName) throws IOException {
+            for (String name : connectionMap.keySet()) {
+                if (!name.equals(userName)) {
+                    connection.send(new Message(MessageType.USER_ADDED, name));
+                }
+            }
+        }
+        private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = connection.receive();
+                if (message.getType() == MessageType.TEXT) {
+                    String toSend = userName + ": " + message.getData();
+                    sendBroadcastMessage(new Message(MessageType.TEXT, toSend));
+                } else {
+                    ConsoleHelper.writeMessage("Error!");
+                }
             }
         }
     }
