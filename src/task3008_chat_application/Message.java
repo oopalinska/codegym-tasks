4c2690dce49a495fdba816db59c735e4d702c603
OlgaOpalinska <olgaopalinska@wp.pl>
2021-06-16 12:21:46 +0200

    Add task 3008 - first classes and simple ConsoleHelper implementation

---
+++ src/task3008_chat_application/Message.java
@@ -0,0 +1,4 @@
+package task3008_chat_application;
+
+public class Message {
+}
---
+++ src/task3008_chat_application/Server.java
@@ -0,0 +1,8 @@
+package task3008_chat_application;
+
+public class Server {
+
+    public static void main(String[] args) {
+
+    }
+}
