85e5730b1e9b37654252ed21e654b30f54e06349
OlgaOpalinska <olgaopalinska@wp.pl>
2021-06-16 13:22:44 +0200

    Write Server class supporting multiple simultaneous connections

--- src/task3008_chat_application/Server.java
+++ src/task3008_chat_application/Server.java
@@ -1,8 +1,44 @@
 package task3008_chat_application;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Server {
+    private static ServerSocket serverSocket;
+    private static final Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
+        final int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            ConsoleHelper.writeMessage("Server is running...");
+            while(true) {
+                final Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage(e.getMessage());
+        }
+    }
 
+    public static void sendBroadcastMessage(Message message) {
+        for (Connection connection : connectionMap.values()) {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("The message couldn't be sent.");
+            }
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(final Socket socket) {
+            this.socket = socket;
+        }
     }
 }
+
