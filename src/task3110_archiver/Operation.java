2f4258595259beaafeebf24d214eae47a171094b
OlgaOpalinska <olgaopalinska@wp.pl>
2021-06-28 10:41:51 +0200

    Implement the createZip(Path source) method, ConsoleHelper and Operation enum

---
+++ src/task3110_archiver/Operation.java
@@ -0,0 +1,10 @@
+package task3110_archiver;
+
+public enum Operation {
+    CREATE,
+    ADD,
+    REMOVE,
+    EXTRACT,
+    CONTENT,
+    EXIT
+}
--- src/task3110_archiver/ZipFileManager.java
+++ src/task3110_archiver/ZipFileManager.java
@@ -1,6 +1,10 @@
 package task3110_archiver;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -8,7 +12,17 @@ public class ZipFileManager {
     public ZipFileManager(final Path zipFile) {
         this.zipFile = zipFile;
     }
-    public void createZip(Path source) throws Exception { }
+
+    public void createZip(Path source) throws Exception {
+        try(ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+            InputStream inputStream = Files.newInputStream(source)) {
+            zipOutputStream.putNextEntry(new ZipEntry(source.getFileName().toString()));
+            while (inputStream.available() > 0) {
+                zipOutputStream.write(inputStream.read());
+            }
+            zipOutputStream.closeEntry();
+        }
+    }
 
 
 }
