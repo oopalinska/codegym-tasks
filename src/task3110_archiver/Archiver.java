314b16f82a7d961ecd318ea684934a78e986d2b9
OlgaOpalinska <olgaopalinska@wp.pl>
2021-06-28 12:59:29 +0200

    Implement logic for Archiver and FileManager

--- src/task3110_archiver/Archiver.java
+++ src/task3110_archiver/Archiver.java
@@ -1,23 +1,31 @@
 package task3110_archiver;
 
-import task3110_archiver.command.ExitCommand;
-
-import java.nio.file.Paths;
-import java.util.Scanner;
+import task3110_archiver.exception.NoSuchZipFileException;
+import java.io.IOException;
 
 public class Archiver {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the full path to the archive:");
-        final String path = scanner.nextLine();
-        final ZipFileManager zipFileManager = new ZipFileManager(Paths.get(path));
-        System.out.println("Please enter the path to the file to be zipped:");
-        final String fileToZip = scanner.nextLine();
-        try {
-            zipFileManager.createZip(Paths.get(fileToZip));
-            new ExitCommand().execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Operation operation = null;
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (NoSuchZipFileException e) {
+                ConsoleHelper.writeMessage("You didn't select an archive or you selected an invalid file.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("An error occurred. Please check the entered data.");
+            }
+        } while (operation != Operation.EXIT);
+    }
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Select an operation:\n" +
+                "0 - Zip files into an archive\n" +
+                "1 - Add a file to an archive\n" +
+                "2 - Remove a file from an archive\n" +
+                "3 - Extract an archive\n" +
+                "4 - View the contents of an archive\n" +
+                "5 - Exit");
+        final int chosen = ConsoleHelper.readInt();
+        return Operation.values()[chosen];
     }
 }
