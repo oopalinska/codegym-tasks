125d379c77cec37c9f417bfb53636073f7b4ef7f
OlgaOpalinska <olgaopalinska@wp.pl>
2021-06-28 09:34:59 +0200

    Create basic classes: Archiver and ZipFileManager

---
+++ src/task3110_archiver/Archiver.java
@@ -0,0 +1,21 @@
+package task3110_archiver;
+
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Archiver {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the full path to the archive:");
+        final String path = scanner.nextLine();
+        final ZipFileManager zipFileManager = new ZipFileManager(Paths.get(path));
+        System.out.println("Please enter the path to the file to be zipped:");
+        final String fileToZip = scanner.nextLine();
+        try {
+            zipFileManager.createZip(Paths.get(fileToZip));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/task3110_archiver/ZipFileManager.java
@@ -0,0 +1,14 @@
+package task3110_archiver;
+
+import java.nio.file.Path;
+
+public class ZipFileManager {
+    private Path zipFile;
+
+    public ZipFileManager(final Path zipFile) {
+        this.zipFile = zipFile;
+    }
+    public void createZip(Path source) throws Exception { }
+
+
+}
