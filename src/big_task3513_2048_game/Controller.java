40be2d0325bc5f5ae78b41cdd5fa02caef20da77
OlgaOpalinska <olgaopalinska@wp.pl>
2021-07-21 14:39:33 +0200

    Logic for keyPressed() in Controller

--- src/big_task3513_2048_game/Controller.java
+++ src/big_task3513_2048_game/Controller.java
@@ -1,10 +1,17 @@
 package big_task3513_2048_game;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Controller extends KeyAdapter {
     private Model model;
     private View view;
+    private final static int WINNING_TILE = 2048;
+
+    public Controller(final Model model) {
+        this.model = model;
+        this.view = new View(this);
+    }
 
     public Tile[][] getGameTiles() {
         return model.getGameTiles();
@@ -12,5 +19,42 @@ public class Controller extends KeyAdapter {
 
     public int getScore() {
         return model.score;
+    }
+
+    public void resetGame() {
+        model.score = 0;
+        view.isGameWon = false;
+        view.isGameLost = false;
+        model.resetGameTiles();
+    }
+
+    @Override
+    public void keyPressed(final KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            resetGame();
+        }
+        if (!model.canMove()) {
+            view.isGameLost = true;
+        }
+        if (!view.isGameWon && !view.isGameLost) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    model.left();
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    model.right();
+                    break;
+                case KeyEvent.VK_DOWN:
+                    model.down();
+                    break;
+                case KeyEvent.VK_UP:
+                    model.up();
+                    break;
+            }
+        }
+        if (model.maxTile == WINNING_TILE) {
+            view.isGameWon = true;
+        }
+        view.repaint();
     }
 }
