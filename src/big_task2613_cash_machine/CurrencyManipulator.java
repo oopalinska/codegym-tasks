587b508131f036aaa6d25bdc174040d057f9736a
OlgaOpalinska <olgaopalinska@wp.pl>
2021-08-17 17:38:06 +0200

    Implement WithdrawCommand

--- src/big_task2613_cash_machine/CurrencyManipulator.java
+++ src/big_task2613_cash_machine/CurrencyManipulator.java
@@ -1,7 +1,8 @@
 package big_task2613_cash_machine;
 
-import java.util.HashMap;
-import java.util.Map;
+import big_task2613_cash_machine.exception.InsufficientFundsException;
+
+import java.util.*;
 
 public class CurrencyManipulator {
     private String currencyCode;
@@ -23,6 +24,7 @@ public class CurrencyManipulator {
             denominations.put(denomination, count);
         }
     }
+
     public int getTotalAmount() {
         int totalAmount = 0;
         for (Map.Entry<Integer, Integer> entry : denominations.entrySet()) {
@@ -30,7 +32,58 @@ public class CurrencyManipulator {
         }
         return totalAmount;
     }
+
     public boolean hasMoney() {
         return !this.denominations.isEmpty();
     }
+
+    public boolean isAmountAvailable(int expectedAmount) {
+        return getTotalAmount() >= expectedAmount;
+    }
+
+    public Map<Integer, Integer> withdrawAmount(int expectedAmount) throws InsufficientFundsException {
+        int amountLeft = expectedAmount;
+        Map<Integer, Integer> denominationsCopy = new HashMap<>(denominations);
+        ArrayList<Integer> keys = getSortedAndReversedKeysArray();
+
+        TreeMap<Integer, Integer> resultMap = new TreeMap<>(Comparator.reverseOrder());
+
+        for (Integer denomination : keys) {
+            Integer howManyCurrents = denominationsCopy.get(denomination);
+            while (true) {
+                if (amountLeft < denomination || howManyCurrents == 0) {
+                    denominationsCopy.put(denomination, howManyCurrents);
+                    break;
+                }
+                amountLeft -= denomination;
+                howManyCurrents--;
+
+                if (resultMap.containsKey(denomination)) {
+                    resultMap.put(denomination, resultMap.get(denomination) + 1);
+                } else {
+                    resultMap.put(denomination, 1);
+                }
+            }
+        }
+        refreshDenominationsIfOperationSuccess(amountLeft, denominationsCopy);
+
+        return resultMap;
+    }
+
+    private void refreshDenominationsIfOperationSuccess(int amountLeft, Map<Integer,Integer> denominationsCopy) throws InsufficientFundsException {
+        if (amountLeft > 0) {
+            throw new InsufficientFundsException();
+        } else {
+            denominations.clear();
+            denominations.putAll(denominationsCopy);
+        }
+    }
+
+    private ArrayList<Integer> getSortedAndReversedKeysArray() {
+        ArrayList<Integer> keys = new ArrayList<>(denominations.keySet());
+        Collections.sort(keys);
+        Collections.reverse(keys);
+        return keys;
+    }
 }
+
--- src/big_task2613_cash_machine/command/WithdrawCommand.java
+++ src/big_task2613_cash_machine/command/WithdrawCommand.java
@@ -1,8 +1,37 @@
 package big_task2613_cash_machine.command;
 
+import big_task2613_cash_machine.ConsoleHelper;
+import big_task2613_cash_machine.CurrencyManipulator;
+import big_task2613_cash_machine.CurrencyManipulatorFactory;
+import big_task2613_cash_machine.exception.InsufficientFundsException;
+import big_task2613_cash_machine.exception.InterruptedOperationException;
+
+import java.util.Map;
+
 class WithdrawCommand implements Command {
     @Override
-    public void execute() {
-
+    public void execute() throws InterruptedOperationException {
+        String currencyCode = ConsoleHelper.requestCurrencyCode();
+        CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
+        while (true) {
+            ConsoleHelper.writeMessage("Please enter the amount to withdraw:");
+            String answer = ConsoleHelper.readString();
+            try {
+                int amount = Integer.parseInt(answer);
+                if (manipulator.isAmountAvailable(amount)) {
+                    Map<Integer, Integer> withdrawn = manipulator.withdrawAmount(amount);
+                    for (Map.Entry<Integer, Integer> entry : withdrawn.entrySet()) {
+                        ConsoleHelper.writeMessage(String.format("\t%d - %d", entry.getKey(), entry.getValue()));
+                    }
+                    ConsoleHelper.writeMessage("Your transaction was successful!");
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                ConsoleHelper.writeMessage("Your data is invalid. Try again.");
+            } catch (InsufficientFundsException ex) {
+                ConsoleHelper.writeMessage("We are very sorry - the banknotes in this ATM are insufficient to perform this withdrawal...");
+            }
+        }
     }
 }
+
