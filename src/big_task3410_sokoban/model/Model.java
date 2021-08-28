3f206e52eeaac1a02583880b16d9f39f601b72c0
OlgaOpalinska <olgaopalinska@wp.pl>
2021-08-28 11:04:40 +0200

    Checking collisions and moving game objects (methods in Model)

--- src/big_task3410_sokoban/model/Model.java
+++ src/big_task3410_sokoban/model/Model.java
@@ -5,6 +5,8 @@ import big_task3410_sokoban.controller.EventListener;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 
+import static big_task3410_sokoban.model.GameObject.BOARD_CELL_SIZE;
+
 public class Model {
     private EventListener eventListener;
     private GameObjects gameObjects;
@@ -42,5 +44,33 @@ public class Model {
 
     public void move(final Direction direction) {
 
+    }
+
+    public boolean checkWallCollision(CollisionObject gameObject, Direction direction) {
+        for (Wall wall : gameObjects.getWalls()) {
+            if (wall.isCollision(gameObject, direction)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkBoxCollisionAndMoveIfAvailable(Direction direction) {
+        for (Box box : gameObjects.getBoxes()) {
+            if (gameObjects.getPlayer().isCollision(box, direction)) {
+                for (Box nextBox : gameObjects.getBoxes()) {
+                    if (!nextBox.equals(box) && box.isCollision(nextBox, direction)) {
+                        return true;
+                    }
+                }
+                if (checkWallCollision(box, direction)) {
+                    return true;
+                }
+            }
+            int dx = direction == Direction.LEFT ? -BOARD_CELL_SIZE : (direction == Direction.RIGHT ? BOARD_CELL_SIZE : 0);
+            int dy = direction == Direction.UP ? -BOARD_CELL_SIZE : (direction == Direction.DOWN ? BOARD_CELL_SIZE : 0);
+            box.move(dx, dy);
+        }
+        return false;
     }
 }
